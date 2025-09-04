e1b8a8c641f8e62dcac92dfd92dae24ac2cbeb44
Anshit Chauhan <chauhananshit48@gmail.com>
2025-09-05 02:18:22 +0530

    add view for test interface

--- app/startup/serverStartup.js
+++ app/startup/serverStartup.js
@@ -1,6 +1,7 @@
 const userRoutes = require('../routes/userRoutes');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const questionModel = require('../modals/questionModel');
 
 const serverStartup = async (app) => {
     app.use(require('express').json());
@@ -9,9 +10,20 @@ const serverStartup = async (app) => {
     app.use(cookieParser());
     app.set('view engine', 'ejs');
     app.set('views', path.join(__dirname, '../views'));
-    app.use('/user',userRoutes);
-    // user routes
-    // app.get('/', require('../routes/userRoutes'));
+    app.use('/user', userRoutes);
+
+    app.get('/test/:difficulty', async (req, res) => {
+        const difficulty = req.params.difficulty;
+
+        const questions = await questionModel.find({ difficulty: new RegExp(`^${difficulty}$`, 'i') });
+
+        res.render('test', {
+            difficulty: difficulty.charAt(0).toUpperCase() + difficulty.slice(1),
+            questions
+        });
+    });
+
+
 }
 
 module.exports = serverStartup;
