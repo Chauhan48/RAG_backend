f43bb4544a9a9f61f073713c4f27d959607363a7
Anshit Chauhan <chauhananshit48@gmail.com>
2025-09-25 19:37:33 +0530

    implemented AI to generate questions from given pdf or youtube video url

--- config/config.js
+++ config/config.js
@@ -4,4 +4,5 @@ module.exports = {
     PORT: process.env.PORT || 3000,
     DB_URI: process.env.DB_URL || '',
     TOKEN_SECRET: process.env.TOKEN_SECRET || '####',
+    API_KEY: process.env.API_KEY
 };
