708ed2f37681fec74ec9903c1c29f12fb817fb06
Anshit Chauhan <chauhananshit48@gmail.com>
2025-09-26 12:19:47 +0530

    refactor: updated cors-options

--- app/startup/serverStartup.js
+++ app/startup/serverStartup.js
@@ -4,14 +4,16 @@ const cookieParser = require('cookie-parser');
 
 const serverStartup = async (app) => {
     app.use(require('express').json());
-    app.use(cors(
-        {
-            origin: [
-                'http://localhost:5173'
-            ],
-            credentials: true
-        }
-    ));
+
+    app.use(cors({
+        origin: [
+            'http://localhost:5173',
+            'https://chauhan48.github.io'
+        ],
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }));
     app.use(cookieParser());
     app.use(require('express').urlencoded({ extended: true }));
     app.use(require('express').static('public'));
